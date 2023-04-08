94e207b6d815b042b83d43c946332a537ae83593
arapbaeva <aiymvector@gmail.com>
2023-04-08 09:10:18 +0600

    added hw4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -18,7 +18,12 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     }
 ) => {
     const finalClassName = s.button
-        + (disabled ? s.disabled : xType === 'red' ? s.red : '' + s.secondary)
+        + (disabled ? " "
+            + s.disabled
+            : xType === 'red' ? ' ' + s.red
+                :  xType === 'secondary' ? " " + s.secondary
+                    : ' ' + s.default)
+
         + (className ? ' ' + className : '');
 // задачка на смешивание классов
 
