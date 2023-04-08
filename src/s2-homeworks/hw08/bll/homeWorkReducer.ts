b27ff399025357784612fb9417ccc2dd55fa94dd
arapbaeva <aiymvector@gmail.com>
2023-04-08 13:50:52 +0600

    fix sort name

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,23 +7,12 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-           return state.sort((a, b) => {
-                    if (a.name > b.name) {
-                        return 1
-                    }
-                    if (b.name > a.name) {
-                        return 1
-                    }
-                    else {
-                        return -1
-                    }
-                    return 0
-                }
-            )// need to fix
+            return action.payload === 'up' ?
+                state.sort((a, b) => a.name > b.name ? 1 : -1)
+                : state.sort((a, b) => a.name < b.name ? 1 : -1)
         }
         case 'check': {
-            return state.filter(el=>el.age>action.payload)
+            return state.filter(el => el.age > action.payload)
         }
         default:
             return state
