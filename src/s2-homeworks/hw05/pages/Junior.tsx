56b61551f23ae43e6f5060d886d0d0942a5202c9
arapbaeva <aiymvector@gmail.com>
2023-04-09 10:16:48 +0600

    added hw9

--- src/s2-homeworks/hw05/pages/Junior.tsx
+++ src/s2-homeworks/hw05/pages/Junior.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HW6 from "src/s2-homeworks/hw06/HW6";
 import HW7 from "src/s2-homeworks/hw07/HW7";
 import HW8 from "src/s2-homeworks/hw08/HW8";
+import HW9 from "src/s2-homeworks/hw09/HW9";
 // import HW6 from '../../hw06/HW6'
 // import HW7 from '../../hw07/HW7'
 // import HW8 from '../../hw08/HW8'
@@ -13,7 +14,7 @@ function Junior() {
             <HW6 />
             <HW7 />
             <HW8 />
-            {/*<HW9 />*/}
+            <HW9 />
         </div>
     )
 }
