be88d02ac190a1cdc76e0cdecd0552590169ea91
arapbaeva <aiymvector@gmail.com>
2023-04-09 11:50:27 +0600

    added hw10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,10 +1,15 @@
 const initState = {
     isLoading: false,
 }
+export type InitialStateType= typeof initState
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType):InitialStateType  => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
+        case 'CHANGE_LOADING':
+            return {
+                ...state, isLoading: action.isLoading
+            }
 
         default:
             return state
