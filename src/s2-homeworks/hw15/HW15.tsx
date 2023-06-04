7f310b56501241a65dfe254e5e88ac3d3864d415
arapbaeva <aiymvector@gmail.com>
2023-06-04 20:13:36 +0600

    fixed hw 15

--- src/s2-homeworks/hw15/HW15.tsx
+++ src/s2-homeworks/hw15/HW15.tsx
@@ -52,9 +52,11 @@ const HW15 = () => {
         getTechs(params)
             .then((res) => {
                 // делает студент
-
-                // сохранить пришедшие данные
-
+              if (res){
+                  setTechs(res.data.techs)
+                  setTotalCount(res.data.totalCount)
+                  setLoading(false)
+              }
                 //
             })
     }
@@ -62,11 +64,11 @@ const HW15 = () => {
     const onChangePagination = (newPage: number, newCount: number) => {
         // делает студент
 
-        // setPage(
-        // setCount(
+        setPage(newPage)
+        setCount(newCount)
 
-        // sendQuery(
-        // setSearchParams(
+        sendQuery({sort: sort, page: newPage, count: newCount})
+
 
         //
     }
@@ -74,11 +76,11 @@ const HW15 = () => {
     const onChangeSort = (newSort: string) => {
         // делает студент
 
-        // setSort(
-        // setPage(1) // при сортировке сбрасывать на 1 страницу
+        setSort(newSort)
+        setPage(1) // при сортировке сбрасывать на 1 страницу
 
-        // sendQuery(
-        // setSearchParams(
+        sendQuery({sort: newSort, page: 1, count:count})
+        // setSearchParams()
 
         //
     }
